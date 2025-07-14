84b18b4559638abaf346b2f3c0ad614d0bc9d679
jinwoo <jinwoo@thinkingdata.kr>
2025-07-14 17:07:47 +0900

    fix: 배포용 config 실제 저장소 정보로 수정

--- docs-onboarding/docusaurus.config.ts
+++ docs-onboarding/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://wo123kr.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/thinking_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wo123kr', // Usually your GitHub org/user name.
+  projectName: 'thinking_docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
